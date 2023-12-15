8cec42d40aafd38b99400e96e2f179951d51ca38
Abhishek Agarwal <abhishek.agarwal@sourcefuse.com>
2023-12-15 17:02:27 +0530

    Initial Commit

---
+++ dest/example.js
@@ -0,0 +1,202 @@
+"use strict";
+var Role;
+(function (Role) {
+    Role["USER"] = "user";
+    Role["ADMIN"] = "admin";
+})(Role || (Role = {}));
+class User {
+    constructor(id, firstName, middleName, lastName, email, phoneNo, role, address) {
+        this.id = id;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNo = phoneNo;
+        this.role = role;
+        this.address = address;
+    }
+    get getId() {
+        return this.id;
+    }
+}
+let userList = [
+    new User(1, "Ryan", "Ten", "Jones", "abc@gmail.com", 1234567890, Role.ADMIN, "64, North Street, LA, Los Angeles"),
+    new User(2, "Seth", "", "Rollin", "def@gmail.com", 1234567890, Role.USER, "64, North Street, LA, Los Angeles"),
+    new User(3, "Faf", "Du", "Plesis", "ghi@gmail.com", 1234567890, Role.USER, "64, North Street, LA, Los Angeles")
+];
+class Utility {
+    createUser(name, email, phoneNo, role, address) {
+        const nameArray = name.trim().split(" ", 3);
+        const firstName = nameArray[0];
+        let middleName, lastName;
+        if (nameArray.length > 2) {
+            middleName = nameArray[1];
+            lastName = nameArray[2];
+        }
+        else if (nameArray.length == 2) {
+            middleName = "";
+            lastName = nameArray[1];
+        }
+        else {
+            middleName = "";
+            lastName = "";
+        }
+        const id = userList.length > 0 ? userList[userList.length - 1].getId + 1 : 1;
+        const roleEnum = role;
+        const newUser = new User(id, firstName, middleName, lastName, email, phoneNo, roleEnum, address);
+        userList.push(newUser);
+        return id;
+    }
+    deleteUser(id) {
+        const idx = userList.findIndex(user => user.getId == id);
+        console.log('Index is ' + idx);
+        if (idx >= 0) {
+            userList.splice(idx, 1);
+        }
+    }
+    getUser(searchKey) {
+        const idx = userList.findIndex(user => user.getId == searchKey);
+        return userList[idx];
+    }
+    updateUser(user) {
+        const id = user.getId;
+        const idx = userList.findIndex(user => user.getId == id);
+        userList[idx] = user;
+        return user.getId;
+    }
+}
+function loadData() {
+    console.log("Inside load Data Function");
+    let refreshBtn = document.getElementById("refreshBtn");
+    refreshBtn.innerHTML = "Refresh Data";
+    changeHTMLElementVisibility("userFormSection", "none");
+    changeHTMLElementVisibility("addUserBtn", "inline-block");
+    changeHTMLElementVisibility("userTable", "block");
+    let tableBody = document.getElementById("userData");
+    let userData = '';
+    userList.forEach((user) => {
+        console.log(user);
+        userData += `
+                <tr>
+                <td>${user.getId}</td>
+                <td>${user.firstName}</td>
+                <td>${user.middleName}</td>
+                <td>${user.lastName}</td>
+                <td>${user.email}</td>
+                <td>${user.phoneNo}</td>
+                <td>${user.role}</td>
+                <td>${user.address}</td>
+                <td>
+                    <div id="editSection">
+                        <button id="editBtn" onClick=editRow(this)>Edit</button>
+                        <button id="delBtn" onClick=deleteRow(this)>Delete</button>
+                    </div>
+                    <div id="saveSection" style>
+                        <button id="saveBtn" onClick=saveChanges(this)>Save</button>
+                        <button id="cancelBtn" onClick=cancelChanges(this)>Cancel</button>
+                    </div>
+                    
+                </td>
+                </tr>
+            `;
+    });
+    tableBody.innerHTML = userData;
+}
+function changeHTMLElementVisibility(id, status) {
+    let element = document.getElementById(id);
+    element.style.display = status;
+}
+function openUserRegistrationForm() {
+    let roleDropDown = document.getElementById("roleDropDown");
+    console.log(roleDropDown.options.length);
+    if (roleDropDown.options.length == 1) {
+        const keys = Object.entries(Role).forEach(([key, value]) => {
+            let option = document.createElement("option");
+            option.setAttribute('value', value);
+            let optionText = document.createTextNode(key);
+            option.appendChild(optionText);
+            roleDropDown.appendChild(option);
+        });
+    }
+    console.log("Insie User Registration Form");
+    changeHTMLElementVisibility("userFormSection", "block");
+    changeHTMLElementVisibility("userTable", "none");
+}
+function closeForm() {
+    let form = document.getElementById("userForm");
+    form.reset();
+    changeHTMLElementVisibility("userFormSection", "none");
+    loadData();
+}
+function createUser() {
+    console.log("Inside User Creation");
+    const nameField = document.getElementById("name");
+    const emailField = document.getElementById("email");
+    const phoneNoField = document.getElementById("phnNo");
+    const roleField = document.getElementById("roleDropDown");
+    const addressField = document.getElementById("address");
+    const name = nameField === null || nameField === void 0 ? void 0 : nameField.value;
+    const email = emailField === null || emailField === void 0 ? void 0 : emailField.value;
+    const phoneNo = parseInt(phoneNoField === null || phoneNoField === void 0 ? void 0 : phoneNoField.value, 10);
+    const role = roleField === null || roleField === void 0 ? void 0 : roleField.value;
+    const address = addressField === null || addressField === void 0 ? void 0 : addressField.value;
+    const obj = new Utility();
+    const id = obj.createUser(name, email, phoneNo, role, address);
+    alert('User has been created with id - ' + id);
+    closeForm();
+}
+function deleteRow(btn) {
+    var _a, _b;
+    let row = (_b = (_a = btn.parentNode) === null || _a === void 0 ? void 0 : _a.parentNode) === null || _b === void 0 ? void 0 : _b.parentNode;
+    let id = parseInt(row.cells[0].textContent);
+    console.log(id);
+    const obj = new Utility();
+    obj.deleteUser(id);
+    alert('User has been deleted successfully');
+    loadData();
+}
+function editRow(btn) {
+    var _a, _b, _c;
+    let row = (_b = (_a = btn.parentNode) === null || _a === void 0 ? void 0 : _a.parentNode) === null || _b === void 0 ? void 0 : _b.parentNode;
+    for (const [idx, cell] of Array.from(row.cells).entries()) {
+        if (idx == 1 || idx == 2 || idx == 3 || idx == 7) {
+            cell.setAttribute("contenteditable", 'true');
+        }
+    }
+    let tableCell = (_c = btn.parentNode) === null || _c === void 0 ? void 0 : _c.parentNode;
+    let editSection = tableCell.childNodes[1];
+    editSection.style.display = "none";
+    let saveSection = tableCell.childNodes[3];
+    saveSection.style.display = "block";
+}
+function saveChanges(btn) {
+    var _a, _b;
+    let row = (_b = (_a = btn.parentNode) === null || _a === void 0 ? void 0 : _a.parentNode) === null || _b === void 0 ? void 0 : _b.parentNode;
+    const id = parseInt(row.cells[0].textContent);
+    const obj = new Utility();
+    let user = obj.getUser(id);
+    console.log('Old User Details ' + JSON.stringify(user));
+    user.firstName = row.cells[1].textContent || " ";
+    user.middleName = row.cells[2].textContent || " ";
+    user.lastName = row.cells[3].textContent || " ";
+    user.address = row.cells[7].textContent || " ";
+    console.log("New User details" + JSON.stringify(user));
+    obj.updateUser(user);
+    cancelChanges(btn);
+}
+function cancelChanges(btn) {
+    var _a, _b, _c;
+    let row = (_b = (_a = btn.parentNode) === null || _a === void 0 ? void 0 : _a.parentNode) === null || _b === void 0 ? void 0 : _b.parentNode;
+    for (const [idx, cell] of Array.from(row.cells).entries()) {
+        if (idx == 1 || idx == 2 || idx == 3 || idx == 7) {
+            cell.removeAttribute("contenteditable");
+        }
+    }
+    let tableCell = (_c = btn.parentNode) === null || _c === void 0 ? void 0 : _c.parentNode;
+    let editSection = tableCell.childNodes[1];
+    editSection.style.display = "block";
+    let saveSection = tableCell.childNodes[3];
+    saveSection.style.display = "none";
+    loadData();
+}
+// export {};
---
+++ src/example.ts
@@ -0,0 +1,266 @@
+enum Role {
+    USER = "user",
+    ADMIN = "admin"
+}
+
+class User {
+    constructor(private id: number,public firstName: string,public middleName: string,
+        public lastName: string, public email: string, public phoneNo: number,
+        public role: Role, public address: string) { }
+
+    get getId(): number {
+        return this.id;
+
+    }
+}
+
+interface Actions {
+    createUser(name: string, email: string, phoneNo: number, role: string, address: string): number;
+    deleteUser(id: number): void;
+    getUser(searchKey: number): User;
+    updateUser(user: User): number;
+}
+let userList: Array<User> = [
+    new User(1,"Ryan","Ten","Jones","abc@gmail.com",1234567890,Role.ADMIN,"64, North Street, LA, Los Angeles"),
+    new User(2,"Seth","","Rollin","def@gmail.com",1234567890,Role.USER,"64, North Street, LA, Los Angeles"),
+    new User(3,"Faf","Du","Plesis","ghi@gmail.com",1234567890,Role.USER,"64, North Street, LA, Los Angeles")];
+
+class Utility implements Actions{
+    
+    createUser(name: string, email: string, phoneNo: number, role: string, address: string): number{
+
+        const nameArray = name.trim().split(" ",3);
+        const firstName = nameArray[0];
+        let middleName: string,lastName: string;
+        if(nameArray.length>2)
+        {
+            middleName = nameArray[1];
+            lastName = nameArray[2];
+        }
+        else if(nameArray.length==2){
+            middleName = "";
+            lastName = nameArray[1];
+        }
+        else{
+            middleName = "";
+            lastName = "";
+        }
+
+
+        const id = userList.length>0?userList[userList.length-1].getId+1:1;
+        const roleEnum = role as Role;
+
+
+        const newUser = new User(id,firstName,middleName,lastName,email,phoneNo,roleEnum,address);
+        userList.push(newUser);
+        return id;
+    }
+
+    deleteUser(id: number): void {
+        const idx = userList.findIndex(user => user.getId==id);
+
+        console.log('Index is '+idx);
+        if(idx>=0)
+        {
+            userList.splice(idx,1);
+        }
+    }
+
+    getUser(searchKey: number): User {  
+        const idx = userList.findIndex(user => user.getId==searchKey);
+        return userList[idx];
+    }
+
+    updateUser(user: User): number {
+
+        const id = user.getId;
+        const idx = userList.findIndex(user => user.getId==id);
+        userList[idx] = user;
+        return user.getId;
+    }
+}
+
+
+
+
+
+function loadData(): void {
+    console.log("Inside load Data Function");
+    let refreshBtn = <HTMLElement>document.getElementById("refreshBtn");
+    refreshBtn.innerHTML = "Refresh Data"
+
+    changeHTMLElementVisibility("userFormSection", "none");
+    changeHTMLElementVisibility("addUserBtn", "inline-block");
+
+    changeHTMLElementVisibility("userTable", "block");
+
+    let tableBody = <HTMLElement>document.getElementById("userData");
+    let userData = '';
+    userList.forEach((user) => {
+        console.log(user);
+        
+        userData += `
+                <tr>
+                <td>${user.getId}</td>
+                <td>${user.firstName}</td>
+                <td>${user.middleName}</td>
+                <td>${user.lastName}</td>
+                <td>${user.email}</td>
+                <td>${user.phoneNo}</td>
+                <td>${user.role}</td>
+                <td>${user.address}</td>
+                <td>
+                    <div id="editSection">
+                        <button id="editBtn" onClick=editRow(this)>Edit</button>
+                        <button id="delBtn" onClick=deleteRow(this)>Delete</button>
+                    </div>
+                    <div id="saveSection" style>
+                        <button id="saveBtn" onClick=saveChanges(this)>Save</button>
+                        <button id="cancelBtn" onClick=cancelChanges(this)>Cancel</button>
+                    </div>
+                    
+                </td>
+                </tr>
+            `;
+    });
+    tableBody.innerHTML = userData;
+}
+
+function changeHTMLElementVisibility(id: string, status: string): void {
+
+    let element = <HTMLElement>document.getElementById(id);
+    element.style.display = status;
+}
+
+function openUserRegistrationForm(): void {
+
+    let roleDropDown = <HTMLSelectElement>document.getElementById("roleDropDown");
+    
+    console.log(roleDropDown.options.length);
+    if(roleDropDown.options.length==1)
+    {
+        const keys = Object.entries(Role).forEach(([key,value]) => {
+        
+            let option = <HTMLElement>document.createElement("option");
+            option.setAttribute('value', value);
+          
+            let optionText = document.createTextNode(key);
+            option.appendChild(optionText);
+          
+            roleDropDown.appendChild(option);
+        });
+    }
+    console.log("Insie User Registration Form");
+    changeHTMLElementVisibility("userFormSection", "block");
+
+    changeHTMLElementVisibility("userTable", "none");
+}
+
+function closeForm(): void {
+
+    let form = <HTMLFormElement>document.getElementById("userForm");
+    form.reset();
+    changeHTMLElementVisibility("userFormSection", "none");
+
+    loadData();
+}
+
+function createUser(){
+    console.log("Inside User Creation");
+
+    const nameField = <HTMLInputElement>document.getElementById("name");
+    const emailField = <HTMLInputElement>document.getElementById("email");
+    const phoneNoField = <HTMLInputElement>document.getElementById("phnNo");
+    const roleField = <HTMLSelectElement>document.getElementById("roleDropDown");
+    const addressField = <HTMLInputElement>document.getElementById("address")
+
+    const name = nameField?.value;
+    const email = emailField?.value;
+    const phoneNo = parseInt(phoneNoField?.value,10);
+    const role = roleField?.value;
+    const address = addressField?.value;
+
+    const obj = new Utility();
+    const id = obj.createUser(name,email,phoneNo,role,address);
+
+    alert('User has been created with id - '+ id);
+
+    closeForm();
+
+}
+
+function deleteRow(btn: HTMLButtonElement){
+
+    let row = btn.parentNode?.parentNode?.parentNode as HTMLTableRowElement;
+    let id = parseInt(row.cells[0].textContent!);
+    console.log(id);
+    const obj = new Utility();
+    obj.deleteUser(id);
+    alert('User has been deleted successfully');
+    loadData();
+}
+
+function editRow(btn: HTMLButtonElement){
+
+    let row = btn.parentNode?.parentNode?.parentNode as HTMLTableRowElement;
+    
+    for(const [idx,cell] of Array.from(row.cells).entries() )
+    {
+        if(idx ==1 || idx==2 || idx==3 || idx==7)
+        {
+            cell.setAttribute("contenteditable",'true');
+        }
+    }
+
+    let tableCell = btn.parentNode?.parentNode as HTMLTableCellElement;
+    
+    let editSection = tableCell.childNodes[1] as HTMLDivElement;
+    editSection.style.display = "none"
+    let saveSection = tableCell.childNodes[3] as HTMLDivElement;
+    saveSection.style.display = "block"
+}
+
+function saveChanges(btn: HTMLButtonElement){
+
+    let row = btn.parentNode?.parentNode?.parentNode as HTMLTableRowElement;
+    const id = parseInt(row.cells[0].textContent!);
+    
+    const obj = new Utility();
+    let user = obj.getUser(id);
+    console.log('Old User Details ' + JSON.stringify(user));
+    user.firstName = row.cells[1].textContent || " ";
+    user.middleName = row.cells[2].textContent || " ";
+    user.lastName = row.cells[3].textContent || " ";
+    user.address = row.cells[7].textContent || " ";
+
+    console.log("New User details"+ JSON.stringify(user));
+
+    obj.updateUser(user);
+    cancelChanges(btn);
+}
+
+function cancelChanges(btn: HTMLButtonElement){
+
+    let row = btn.parentNode?.parentNode?.parentNode as HTMLTableRowElement;
+    
+    for(const [idx,cell] of Array.from(row.cells).entries() )
+    {
+        if(idx ==1 || idx==2 || idx==3 || idx==7)
+        {
+            cell.removeAttribute("contenteditable");
+        }
+    }
+    let tableCell = btn.parentNode?.parentNode as HTMLTableCellElement;
+    
+    let editSection = tableCell.childNodes[1] as HTMLDivElement;
+    editSection.style.display = "block"
+    let saveSection = tableCell.childNodes[3] as HTMLDivElement;
+    saveSection.style.display = "none"
+
+    loadData();
+
+}
+
+
+
+// export {};
